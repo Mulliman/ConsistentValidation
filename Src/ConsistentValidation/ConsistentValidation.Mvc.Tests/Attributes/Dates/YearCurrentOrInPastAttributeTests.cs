cc5511c0cf3bda7d27563a2a60b62ef5790e9a1d
Sam Mullins <sammullins.co.uk@gmail.com>
2016-04-16 19:05:11 +0100

    Year in past and current year or in past attributes and tests.

---
+++ Src/ConsistentValidation/ConsistentValidation.Mvc.Tests/Attributes/Dates/YearCurrentOrInPastAttributeTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsistentValidation.Mvc.Attributes.Dates;
+using System;
+
+namespace ConsistentValidation.Mvc.Tests.Attributes.Dates
+{
+    [TestClass]
+    public class YearCurrentOrInPastAttributeTests : AttributeTestBase
+    {
+        public class YearStringModel
+        {
+            [ConsistentYearCurrentOrInPast]
+            public string YearToTest { get; set; }
+        }
+
+        public class YearIntModel
+        {
+            [ConsistentYearCurrentOrInPast]
+            public int? YearToTest { get; set; }
+        }
+
+        #region Valid Cases
+
+        [TestMethod]
+        public void YearCurrentOrInPastAttributeTests_WhenYearToValidateIsValid_FromString_ItShouldBeValid()
+        {
+            var model = new YearStringModel
+            {
+                YearToTest = (DateTime.Now.Year - 1).ToString()
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(!errors.Any());
+        }
+
+        [TestMethod]
+        public void YearCurrentOrInPastAttributeTests_WhenYearToValidateIsValid_FromInt_ItShouldBeValid()
+        {
+            var model = new YearIntModel
+            {
+                YearToTest = DateTime.Now.Year - 1
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(!errors.Any());
+        }
+
+        #endregion
+
+        #region Invalid Cases
+
+        [TestMethod]
+        public void YearCurrentOrInPastAttributeTests_WhenYearToValidateInPast_FromString_ItShouldNotBeValid()
+        {
+            var model = new YearStringModel
+            {
+                YearToTest = "3000"
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(errors.Any());
+        }
+
+        [TestMethod]
+        public void YearCurrentOrInPastAttributeTests_WhenYearToValidateInPast_FromInt_ItShouldNotBeValid()
+        {
+            var model = new YearIntModel
+            {
+                YearToTest = 3000
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(errors.Any());
+        }
+
+        #endregion
+
+        #region Boundary Cases
+
+        [TestMethod]
+        public void YearCurrentOrInPastAttributeTests_WhenYearToValidateIsThisYear_FromString_ItShouldBeValid()
+        {
+            var model = new YearStringModel
+            {
+                YearToTest = (DateTime.Now.Year).ToString()
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(!errors.Any());
+        }
+
+        [TestMethod]
+        public void YearCurrentOrInPastAttributeTests_WhenYearToValidateIsThisYear_FromInt_ItShouldBeValid()
+        {
+            var model = new YearIntModel
+            {
+                YearToTest = DateTime.Now.Year
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(!errors.Any());
+        }
+
+        # endregion
+    }
+}
---
+++ Src/ConsistentValidation/ConsistentValidation.Mvc.Tests/Attributes/Dates/YearInPastAttributeTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsistentValidation.Mvc.Attributes.Dates;
+using System;
+
+namespace ConsistentValidation.Mvc.Tests.Attributes.Dates
+{
+    [TestClass]
+    public class YearInPastAttributeTests : AttributeTestBase
+    {
+        public class YearStringModel
+        {
+            [ConsistentYearInPast]
+            public string YearToTest { get; set; }
+        }
+
+        public class YearIntModel
+        {
+            [ConsistentYearInPast]
+            public int? YearToTest { get; set; }
+        }
+
+        #region Valid Cases
+
+        [TestMethod]
+        public void YearInPastAttributeTests_WhenYearToValidateIsValid_FromString_ItShouldBeValid()
+        {
+            var model = new YearStringModel
+            {
+                YearToTest = (DateTime.Now.Year - 1).ToString()
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(!errors.Any());
+        }
+
+        [TestMethod]
+        public void YearInPastAttributeTests_WhenYearToValidateIsValid_FromInt_ItShouldBeValid()
+        {
+            var model = new YearIntModel
+            {
+                YearToTest = DateTime.Now.Year - 1
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(!errors.Any());
+        }
+
+        #endregion
+
+        #region Invalid Cases
+
+        [TestMethod]
+        public void YearInPastAttributeTests_WhenYearToValidateInPast_FromString_ItShouldNotBeValid()
+        {
+            var model = new YearStringModel
+            {
+                YearToTest = "3000"
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(errors.Any());
+        }
+
+        [TestMethod]
+        public void YearInPastAttributeTests_WhenYearToValidateInPast_FromInt_ItShouldNotBeValid()
+        {
+            var model = new YearIntModel
+            {
+                YearToTest = 3000
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(errors.Any());
+        }
+
+        #endregion
+
+        #region Boundary Cases
+
+        [TestMethod]
+        public void YearInPastAttributeTests_WhenYearToValidateIsThisYear_FromString_ItShouldNotBeValid()
+        {
+            var model = new YearStringModel
+            {
+                YearToTest = (DateTime.Now.Year).ToString()
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(errors.Any());
+        }
+
+        [TestMethod]
+        public void YearInPastAttributeTests_WhenYearToValidateIsThisYear_FromInt_ItShouldNotBeValid()
+        {
+            var model = new YearIntModel
+            {
+                YearToTest = DateTime.Now.Year
+            };
+
+            var errors = ValidateModel(model);
+
+            Assert.IsTrue(errors.Any());
+        }
+
+        # endregion
+    }
+}
